bb6f83930c3ee8f93b042f883a55f1a14be79263
Nero-Link <nerolink1107@gmail.com>
2022-07-08 18:36:27 +1000

    async data complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useDispatch } from "react";
+// import { useDispatch } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 import URI from "urijs";
-import swagger, { clearCache } from "swagger-client";
+import swagger from "swagger-client";
 import * as CryptoJS from "crypto-js";
 
 const App = () => {
@@ -30,27 +30,32 @@ const App = () => {
 
   const devid = "3002174";
   const apikey = "24030e89-d965-465f-8c63-f2e8072a3e89";
-
   const ptvClient = ptvapi(devid, apikey);
-  let next5 = [];
-  let departures = [];
   let route = "";
   let service = "";
+  let departures = [];
 
-  const getRoute = ptvClient
-    .then((apis) => {
-      return apis.Routes.Routes_RouteFromId({ route_id: 3 });
-    })
-    .then((res) => {
-      route = res.body.route.route_name;
-      service = res.body.route.route_service_status.description;
-      getDepartures();
-    })
-    .catch(console.error);
+  const getRoute = async () => {
+    try {
+      await ptvClient
+        .then((apis) => {
+          return apis.Routes.Routes_RouteFromId({ route_id: 3 });
+        })
+        .then((res) => {
+          route = res.body.route.route_name;
+          service = res.body.route.route_service_status.description;
+          console.log(route);
+          console.log(service);
+          getDepartures();
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const getDepartures = async () => {
     try {
-      ptvClient
+      await ptvClient
         .then((apis) => {
           return apis.Departures.Departures_GetForStopAndRoute({
             route_id: 3,
@@ -59,17 +64,18 @@ const App = () => {
           });
         })
         .then((res) => {
-          departures = res.body;
           let counter = 0;
-          departures.departures.forEach((departure) => {
+          res.body.departures.forEach((departure) => {
             if (
               new Date(departure.scheduled_departure_utc).getTime() >
               new Date().getTime() + 10 * 60000
             ) {
               if (departure.direction_id === 2) counter++;
               if (counter > 0 && counter <= 5 && departure.direction_id === 2) {
-                //  try{ await getDisruptions(departure.disruption_ids);} catch(error){}
-                next5.push({
+                const disruptions = async () =>
+                  await getDisruptions(departure.disruption_ids);
+                disruptions();
+                departures.push({
                   count: counter,
                   time: new Date(
                     departure.scheduled_departure_utc
@@ -77,43 +83,69 @@ const App = () => {
                   platform: departure.platform_number,
                   disruptions: {
                     id: departure.disruption_ids,
-                    // title: `${disruption}`,
-                    title: `${getDisruptions(departure.disruption_ids)}`,
+                    title: [],
                   },
                 });
               }
             }
           });
-          console.log("Next 5 departures: ");
-          console.log(next5);
         });
     } catch (error) {
       console.log(error);
     }
   };
 
-  const getDisruptions = async (obj) =>
-    obj.forEach((id) =>
-      ptvClient
-        .then((apis) => {
-          return apis.Disruptions.Disruptions_GetDisruptionById({
-            disruption_id: `${id}`,
-          });
-        })
-        .then((res) => {
-          console.log("Disruptions: ");
-          id = res.body.disruption.title;
-          console.log(id);
-        })
-        .catch(console.error)
-    );
+  const getDisruptions = async (obj) => {
+    try {
+      await obj.forEach((id) =>
+        ptvClient
+          .then((apis) => {
+            return apis.Disruptions.Disruptions_GetDisruptionById({
+              disruption_id: `${id}`,
+            });
+          })
+          .then((res) => {
+            console.log("Disruptions: ");
+            let title = res.body.disruption.title;
+            console.log(title);
+            console.log("Next");
+            setNext5(id, title);
+          })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const setNext5 = (id, disruption) => {
+    console.log(departures);
+    console.log("Begin");
+    departures.forEach((departure) => {
+      console.log("Starting...");
+      departure.disruptions.id.forEach(() => {
+        if (departure.disruptions.id.includes(id)) {
+          if (!departure.disruptions.title.includes(disruption))
+            departure.disruptions.title.push(disruption);
+        }
+      });
+    });
+    console.log("Next 5 departures: ");
+    console.log(departures);
+  };
+
+  getRoute();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {route}: {service}
+        </p>
+        <p>
+          {departures.map((departure) => {
+            return departures;
+          })}
         </p>
         <a
           className="App-link"
