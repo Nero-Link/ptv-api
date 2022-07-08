a39ae974614f7f8e9007b0988b07d9ac61e0e201
Nero-Link <nerolink1107@gmail.com>
2022-07-08 16:37:20 +1000

    all data coming thru

--- src/App.js
+++ src/App.js
@@ -1,7 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import URI from "urijs";
+import swagger, { clearCache } from "swagger-client";
+import * as CryptoJS from "crypto-js";
+
+const App = () => {
+  // const dispatch = useDispatch();
+  function createSignature(path, key) {
+    return CryptoJS.HmacSHA1(path, key).toString().toUpperCase();
+  }
+
+  function ptvapi(devid, apikey) {
+    return swagger({
+      url: "https://timetableapi.ptv.vic.gov.au/v3/",
+      spec: require("./ptv-openapi.json"),
+      requestInterceptor: function (req) {
+        let url = URI(req.url).addQuery({ devid: devid });
+        let signature = createSignature(
+          url.toString().replace(/.*ptv.vic.gov.au/, ""),
+          apikey
+        );
+        req.url = url.addQuery({ signature: signature }).toString();
+        return req;
+      },
+    }).then((client) => client.apis);
+  }
+
+  const devid = "3002174";
+  const apikey = "24030e89-d965-465f-8c63-f2e8072a3e89";
+
+  const ptvClient = ptvapi(devid, apikey);
+  let next5 = [];
+  let departures = [];
+  let route = "";
+  let service = "";
+
+  const getRoute = ptvClient
+    .then((apis) => {
+      return apis.Routes.Routes_RouteFromId({ route_id: 3 });
+    })
+    .then((res) => {
+      route = res.body.route.route_name;
+      service = res.body.route.route_service_status.description;
+      getDepartures();
+    })
+    .catch(console.error);
+
+  const getDepartures = async () => {
+    try {
+      ptvClient
+        .then((apis) => {
+          return apis.Departures.Departures_GetForStopAndRoute({
+            route_id: 3,
+            route_type: 0,
+            stop_id: 1181,
+          });
+        })
+        .then((res) => {
+          departures = res.body;
+          let counter = 0;
+          departures.departures.forEach((departure) => {
+            if (
+              new Date(departure.scheduled_departure_utc).getTime() >
+              new Date().getTime() + 10 * 60000
+            ) {
+              if (departure.direction_id === 2) counter++;
+              if (counter > 0 && counter <= 5 && departure.direction_id === 2) {
+                //  try{ await getDisruptions(departure.disruption_ids);} catch(error){}
+                next5.push({
+                  count: counter,
+                  time: new Date(
+                    departure.scheduled_departure_utc
+                  ).toLocaleString(),
+                  platform: departure.platform_number,
+                  disruptions: {
+                    id: departure.disruption_ids,
+                    // title: `${disruption}`,
+                    title: `${getDisruptions(departure.disruption_ids)}`,
+                  },
+                });
+              }
+            }
+          });
+          console.log("Next 5 departures: ");
+          console.log(next5);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getDisruptions = async (obj) =>
+    obj.forEach((id) =>
+      ptvClient
+        .then((apis) => {
+          return apis.Disruptions.Disruptions_GetDisruptionById({
+            disruption_id: `${id}`,
+          });
+        })
+        .then((res) => {
+          console.log("Disruptions: ");
+          id = res.body.disruption.title;
+          console.log(id);
+        })
+        .catch(console.error)
+    );
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +126,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,85 +1,8 @@
-import React from "react";
+import React, { useDispatch } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-import URI from "urijs";
-import swagger from "swagger-client";
-import * as CryptoJS from "crypto-js";
-
-function createSignature(path, key) {
-  return CryptoJS.HmacSHA1(path, key).toString().toUpperCase();
-}
-
-function ptvapi(devid, apikey) {
-  return swagger({
-    url: "https://timetableapi.ptv.vic.gov.au/v3/",
-    spec: require("./ptv-openapi.json"),
-    requestInterceptor: function (req) {
-      let url = URI(req.url).addQuery({ devid: devid });
-      let signature = createSignature(
-        url.toString().replace(/.*ptv.vic.gov.au/, ""),
-        apikey
-      );
-      req.url = url.addQuery({ signature: signature }).toString();
-      return req;
-    },
-  }).then((client) => client.apis);
-}
-
-const devid = "3002174";
-const apikey = "24030e89-d965-465f-8c63-f2e8072a3e89";
-
-const ptvClient = ptvapi(devid, apikey);
-const route = ptvClient
-  .then((apis) => {
-    // ROUTES
-    return apis.Routes.Routes_RouteFromId({ route_id: 3 });
-  })
-  .then((res) => {
-    console.log(res.body);
-  })
-  .catch(console.error);
-
-const stop = ptvClient
-  .then((apis) => {
-    // STOPS
-    return apis.Stops.Stops_StopDetails({
-      stop_id: 1181,
-      route_type: 0,
-    });
-  })
-  .then((res) => {
-    console.log(res.body);
-  })
-  .catch(console.error);
-
-const stopsForRoute = ptvClient
-  .then((apis) => {
-    // STOPS FOR ROUTE
-    return apis.Stops.Stops_StopsForRoute({
-      route_id: 3,
-      route_type: 0,
-    });
-  })
-  .then((res) => {
-    console.log(res.body);
-  })
-  .catch(console.error);
-
-const runs = ptvClient
-  .then((apis) => {
-    // RUNS
-    return apis.Runs.Runs_ForRouteAndRouteType({
-      route_id: 3,
-      route_type: 0,
-    });
-  })
-  .then((res) => {
-    console.log(res.body);
-  })
-  .catch(console.error);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
