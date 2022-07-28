5c71243315b1b33f17186c878196b1d1162c526e
Brad Inch <nerolink1107@gmail.com>
2022-07-28 11:53:31 +1000

    tram departures jsx

--- src/components/departures/tram-departures.component.jsx
+++ src/components/departures/tram-departures.component.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { useState, useEffect, Fragment } from "react";
+import { React, useState, useEffect, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay } from "swiper/core";
 import "swiper/css";
 import "swiper/css/autoplay";
 import Spinner from "../spinner/spinner.component";
@@ -13,11 +13,8 @@ import {
   selectDisruptionsMap,
   selectDisruptionsIsLoading,
 } from "../../sagas/trams/disruptions/disruptions.selector";
-import { Departures } from "../../sagas/trains/departures/departures.types";
-import { Disruptions } from "../../sagas/trains/disruptions/disruptions.types";
 
-let departuresArray: [string, Departures][];
-let disruptionsArray: [string, Disruptions][];
+SwiperCore.use([Autoplay]);
 
 const TramDepartures = ({ route }) => {
   const dispatch = useDispatch();
@@ -29,14 +26,16 @@ const TramDepartures = ({ route }) => {
   const disruptionsMap = useSelector(selectDisruptionsMap);
   const disruptionsIsLoading = useSelector(selectDisruptionsIsLoading);
   const [disruptions, setDisruptions] = useState(disruptionsMap[route]);
+  let departuresArray = [];
+  let disruptionsArray = [];
 
-  const departuresLoop = (array: Departures[]): any => {
+  const departuresLoop = () => {
     if (departuresArray.length === 0)
       departuresArray = Object.entries(departuresMap);
     return;
   };
 
-  const disruptionsLoop = (array: Disruptions[]): any => {
+  const disruptionsLoop = () => {
     if (disruptionsArray.length === 0)
       disruptionsArray = Object.entries(disruptionsMap);
     return;
