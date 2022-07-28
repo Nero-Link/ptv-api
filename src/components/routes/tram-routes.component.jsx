c8213dec9e331b14bcf1fb0b0abc2af6cb39604f
Brad Inch <nerolink1107@gmail.com>
2022-07-28 11:52:35 +1000

    tram routes jsx

--- src/components/routes/tram-routes.component.jsx
+++ src/components/routes/tram-routes.component.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect, Key } from "react";
+import { React, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import Spinner from "../spinner/spinner.component";
 import TramDepartures from "../departures/tram-departures.component";
@@ -7,24 +6,21 @@ import {
   selectRoutesMap,
   selectRoutesIsLoading,
 } from "../../sagas/trams/routes/routes.selector";
-import { Routes } from "../../sagas/trains/routes/routes.types";
-import { location, stops } from "../../routes/config";
-
-let routesArray: [string, Routes][];
+import { location } from "../../routes/config";
 
 const TramRoutes = () => {
   const routesMap = useSelector(selectRoutesMap);
   const isLoading = useSelector(selectRoutesIsLoading);
-  const [routes, setRoutes] = useState(routesMap[stops]);
-
-  const objectLoop = (array: Routes[]): any => {
+  const [routes, setRoutes] = useState(routesMap[location]);
+  let routesArray = [];
+  const objectLoop = () => {
     if (routesArray.length === 0 && routesMap.length > 0)
       routesArray = Object.entries(routesMap);
     return;
   };
 
   useEffect(() => {
-    setRoutes(routesMap[stops]);
+    setRoutes(routesMap[location]);
   }, [routes, routesMap, routesArray]);
 
   return (
@@ -36,9 +32,7 @@ const TramRoutes = () => {
           {objectLoop(routesMap)}
           {routesArray.length > 0 &&
             routesArray.map((route) => {
-              return (
-                <TramDepartures key={route[1].id as Key} route={route[1]} />
-              );
+              return <TramDepartures key={route[1].id} route={route[1]} />;
             })}
         </div>
       )}
